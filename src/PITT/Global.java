a7209edca9a570c880959c904615b51d4304648f
tjdans1114 <tjdans1114@snu.ac.kr>
2018-10-20 15:56:03 +0900

    status code map.

--- src/PITT/Global.java
+++ src/PITT/Global.java
@@ -2,4 +2,14 @@ package PITT;
 
 public class Global {
   public static final int PORT = 1111;
+
+  public static final Map<Integer, String> http_status_map  = new TreeMap<Integer,String>(){
+    {
+      //TODO
+      put(200,"OK");
+      put(400,"Bad Request");
+      put(404,"Not Found");
+      put(501,"Not Implemented");
+    }
+  };
 }
