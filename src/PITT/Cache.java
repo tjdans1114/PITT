11bd9b965947be9226e75784ccdf0d5ec15e233c
tjdans1114 <tjdans1114@snu.ac.kr>
2018-11-09 14:50:54 +0900

    TODO : KIMYEONSU - Cacheing

--- src/PITT/Cache.java
+++ src/PITT/Cache.java
@@ -4,11 +4,18 @@ import java.nio.*;
 
 public class Cache {
   //TODO
+  //define fields. e.g. Map<String, ByteBuffer> ? priorityqueue? idon'know
   public static boolean has(String uri){
+    //TODO
     return false;
   }
 
   public static ByteBuffer get(String uri){
+    //TODO
     return null;
+  }
+
+  public static void maintain(Object new_input){
+    //TODO
   }
 }
