6facba0674200b35839ca8caa5eb8618eda501aa
tjdans1114 <tjdans1114@snu.ac.kr>
2018-11-30 20:32:22 +0900

    updated cache implementations

--- src/PITT/Cache.java
+++ src/PITT/Cache.java
@@ -3,71 +3,71 @@ package PITT;
 import java.nio.*;
 import java.util.*;
 
-public class Cache<K,V> {
-  //TODO
+public class Cache{
+  //TODO : uri -> {last-modified, bytebuffer}
+
+
+  //TODO : Don't reference. you should copy it
   //define fields. e.g. Map<String, ByteBuffer> ? priorityqueue? idon'know
-  private int capacity;
+  private static final int capacity = 1024*1024*1; // max byte size of cell . currently 1MB
+  private static final int cache_size = 10; // total # of cells in cache
+
   private static final float load_factor = 0.75f;
-	private static LinkedHashMap<String,ByteBuffer> cache;
-	private int cache_size;
+	private static LinkedHashMap<String,ByteBuffer> cache = new LinkedHashMap<String,ByteBuffer>();
   int current;
   int limit;
 
-  public Cache(){
-    //default size
-    this.capacity = 100;
-    this.cache_size = 100*1024*1024;
-    this.current = 0;
-//    private int lru_size;
-  }
+  //with static, we don't need constructors
 
-  public Cache(int size) {
-    this.cache_size = size;
-    this.capacity = (int)Math.ceil(this.cache_size/this.load_factor)+1;
-    this.cache = new LinkedHashMap<String,ByteBuffer>(this.capacity, this.load_factor, true){
-      private static final long serialVersionUID =1;
-//      @Override protected boolean remove_eldest (Map.Entry<K,V) eldest){
-//        return size() > Cache.this.cache_size;
-//      }
-    };
-  }
+//  public Cache(){
+//    //default size
+//    this.capacity = 100;
+//    this.cache_size = 100*1024*1024;
+//    this.current = 0;
+////    private int lru_size;
+//  }
+
+//  public Cache(int size) {
+//    this.cache_size = size;
+//    this.capacity = (int)Math.ceil(this.cache_size/this.load_factor)+1;
+//    this.cache = new LinkedHashMap<String,ByteBuffer>(this.capacity, this.load_factor, true){
+//      private static final long serialVersionUID =1;
+////      @Override protected boolean remove_eldest (Map.Entry<K,V) eldest){
+////        return size() > Cache.this.cache_size;
+////      }
+//    };
+
+  /** Main Functions */
   public static boolean has(String uri){
-    //TODO
-//    ByteBuffer value = Cache.get(uri);
-//
-//    if (value ==null){
-//      System.out.println("Cache Miss");
-//      return false;
-//    } else {
-//      System.out.println("Cache Hit");
-//      return true;
-//    }
-    return false;
+    return cache.keySet().contains(uri);
   }
 
   public static ByteBuffer get(String uri){
     //TODO
-    boolean check = has(uri);
-    ByteBuffer value = null;
-    if (check){
-      value = Cache.get(uri);
+    if(has(uri)){
+      return null;
     }
-    return value;
+    return cache.get(uri);
   }
+
+  /** Auxiliary functions */
   public synchronized void clear(){
     this.cache.clear();
   }
   public synchronized int used_size(){
     return this.cache.size();
   }
-  public synchronized Collection<Map.Entry<K,V>> getAll(){
-    //return new ArrayList<Map.Entry<K,V>>(map.entrySet());
-    return null;
+
+  public synchronized Collection<Map.Entry<String,ByteBuffer>> getAll(){
+    return new ArrayList<Map.Entry<String,ByteBuffer>>(cache.entrySet());
+
   }
 
   public static void set(String str, ByteBuffer new_input){
-    //TODO
-//    V value = cache.get(str);
+//    //TODO
+//    ByteBuffer value = cache.get(str);
+//
+//    //if
 //    if (cache.get(str) == null){
 //      System.out.println("new data : adding to cache");
 //      if (value.capacity() > limit){
@@ -95,15 +95,15 @@ public class Cache<K,V> {
 //    ByteBuffer first_value = Cache.get(first_key);
 //    Cache.remove(first_key);
 //    current_size -= first_value.capacity();
-//  }
-//  /*
-//  private void removeEldestEntry(){
-//    boolean isRemove = size() > maxsize;
-//    if(isRemove) {
-//      Object obj = this.get(eldest.)
-//    }
-//  }
-//  */
-//
+  }
+  /*
+  private void removeEldestEntry(){
+    boolean isRemove = size() > maxsize;
+    if(isRemove) {
+      Object obj = this.get(eldest.)
+    }
+  }
+  */
+
   }
 }
